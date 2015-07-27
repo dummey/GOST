c184664e7084787441581f659bd2df48b4e9d229
Ricky Ng <dummey@gmail.com>
2015-07-27 02:35:15 -0500

    Adding google chart jsapi

--- app/main/config/dependencies.rb
+++ app/main/config/dependencies.rb
@@ -11,3 +11,5 @@ component 'bootstrap_jumbotron_theme'
 component 'user_templates'
 
 component 'current_time'
+
+javascript_file "https://www.google.com/jsapi?autoload={'modules':[{'name':'visualization','version':'1.1','packages':['corechart']}]}"
