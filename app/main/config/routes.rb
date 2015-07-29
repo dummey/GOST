995319b8bfca3c3e972bf5dcc926b7707f76448b
Ricky Ng <dummey@gmail.com>
2015-07-28 19:40:00 -0500

    Adds initial scaffold for run_pace.

--- app/main/config/routes.rb
+++ app/main/config/routes.rb
@@ -4,6 +4,8 @@ client '/todos', action: 'todos'
 
 client '/bike_speed', component: 'bike_speed'
 
+client '/run_pace', component: 'run_pace', action: 'run_pace'
+
 # Routes for login and signup, provided by user_templates component gem
 client '/signup', component: 'user_templates', controller: 'signup'
 client '/login', component: 'user_templates', controller: 'login', action: 'index'
